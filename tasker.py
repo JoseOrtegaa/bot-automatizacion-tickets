1a686b2abf79821a75e9463febd7f0111dcae5dd
JoseOrtegaa <josedortega1999@gmail.com>
2024-12-07 16:11:10 +0100

    feat(src): Refactorizo codigo, agrego color a las celdas editadas, agrego excepciones y control de errores.

--- tasker.py
+++ tasker.py
@@ -3,13 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
+from openpyxl.styles import PatternFill
 
 NAME_WORKBOOK = './registers/test_tasker.xlsx'
 
 # Configuración global del WebDriver
-def setup_driver():
+def setupDriver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Ejecutar en modo headless
     options.add_argument("--disable-gpu")
@@ -26,11 +28,10 @@ def executeReadExcel(ws, driver):
                 if cells.hyperlink:
                     url_ticket = cells.hyperlink.target
                     driver.get(url_ticket)  # Navegar a la URL
-                    status = get_ticket_status(driver)
+                    status = getTicketStatus(driver)
                     
                     # Actualizar columnas en la hoja de Excel
-                    ws[f'B{i}'].value = status
-                    ws[f'C{i}'].value = datetime.now()
+                    editStatusExcel(status, i)
                     print(f'Indice:{i} , Status:{status}, Updated:{datetime.now()}')
 
     except Exception as e:
@@ -39,13 +40,36 @@ def executeReadExcel(ws, driver):
         print(f'{datetime.now()} - [TASKER - Fin executeReadExcel]')
 
 # Método para obtener el estado del ticket
-def get_ticket_status(driver):
-    current_status = WebDriverWait(driver, 1).until(
-        EC.presence_of_element_located((By.ID, "current_status_ticket"))
-    )
-    selector_elements = Select(current_status)
-    selected_option = selector_elements.first_selected_option
-    return selected_option.text
+def getTicketStatus(driver):
+    try:
+        current_status = WebDriverWait(driver, 1).until(
+            EC.presence_of_element_located((By.ID, "current_status_ticket"))
+        )
+        selector_elements = Select(current_status)
+        selected_option = selector_elements.first_selected_option
+        return selected_option.text
+    except TimeoutException as e:
+        print(f'{datetime.now()} - [TASKER - ERROR] {str(e)}')
+
+def editStatusExcel(status, i):
+    try:
+        if(isinstance(status, str)):
+            background = PatternFill(start_color="3ee876", end_color="3ee876", fill_type="solid")
+            ws[f'B{i}'].value = status
+            ws[f'C{i}'].value = datetime.now()
+            ws[f'A{i}'].fill = background
+            ws[f'B{i}'].fill = background
+            ws[f'C{i}'].fill = background
+        else:
+             background = PatternFill(start_color="e75151", end_color="e75151", fill_type="solid")
+             ws[f'B{i}'].value = f'[ERROR] - Pagina no encontrada.'
+             ws[f'A{i}'].fill = background
+             ws[f'B{i}'].fill = background
+             ws[f'C{i}'].fill = background
+
+    except Exception as e:
+        print(f'{datetime.now()} - [TASKER - ERROR] {e}')
+
 
 # Inicio del programa
 print(f'{datetime.now()} - [TASKER - Init]')
@@ -53,12 +77,15 @@ wb = load_workbook(filename=NAME_WORKBOOK)
 ws = wb.active
 
 # Reutilización del WebDriver
-driver = setup_driver()
+driver = setupDriver()
 
-# AQUI COLOCAR UNA EXEPCION
 executeReadExcel(ws, driver)
 
 # Guardar y cerrar
 driver.quit()
-wb.save(NAME_WORKBOOK)
-print(f'{datetime.now()} - [TASKER - Fin]')
+
+try:
+    wb.save(NAME_WORKBOOK)
+    print(f'{datetime.now()} - [TASKER - Fin]')
+except Exception as e:
+    print(f'{datetime.now()} - [TASKER - ERROR {e}]') 
